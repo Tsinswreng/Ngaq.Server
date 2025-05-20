a956bb8b144dc41cc1454fc26caaf638eb0be199
Tsinswreng <Tsinswreng@gmail.com>
2025-05-20 17:22:51 +0800

    配Docker與Asp.net

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+#docker build -t ngaq-server:1.0 -f ./Dockerfile ../
+#構建上下文在Ngaq/ 洏不在Ngaq/Ngaq.Server
+# 构建阶段（使用.NET 9 SDK）
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /app
+# 复制解决方案文件（优化层缓存）
+#COPY ["Ngaq.sln", "."]
+#COPY ["src/*/*.csproj", "src/"]
+COPY ["Ngaq.Core/Ngaq.Core.csproj", "Ngaq.Core"]
+COPY ["Ngaq.Server/proj/Directory.Build.props", "Ngaq.Server/proj/"]
+COPY ["Ngaq.Server/proj/Directory.Packages.props", "Ngaq.Server/proj/"]
+COPY ["Ngaq.Server/proj/Ngaq.Biz/Ngaq.Biz.csproj", "Ngaq.Server/proj/Ngaq.Biz"]
+COPY ["Ngaq.Server/proj/Ngaq.Web/Ngaq.Web.csproj", "Ngaq.Server/proj/Ngaq.Web"]
+
+#RUN dotnet new sln -n TeQuaero.Backend
+#RUN dotnet sln add src/**/*.csproj
+# 还原NuGet包
+# 先複製.sln和.csproj文件進行dotnet restore，利用Docker層緩存加速構建 。
+# 避免一次性複製所有文件，防止宿主機路徑結構汙染容器。
+#RUN dotnet restore "TeQuaero.Backend.sln"
+#RUN cd src/TeQuaero.Web/ && dotnet restore TeQuaero.Web.csproj && cd ../..
+RUN cd Ngaq.Server/proj/Ngaq.Web/ && dotnet restore
+# 复制所有源码
+COPY . .
+
+# 发布项目
+RUN dotnet publish "Ngaq.Server/proj/Ngaq.Web/Ngaq.Web.csproj" -c Release -o /app/publish
+
+# 运行阶段（使用ASP.NET运行时）
+FROM mcr.microsoft.com/dotnet/aspnet:9.0
+WORKDIR /app
+COPY --from=build /app/publish .
+# 暴露端口（根据实际项目端口配置）
+EXPOSE 2341
+ENTRYPOINT ["dotnet", "Ngaq.Web.dll"]
--- proj/Ngaq.Web/Program.cs
+++ proj/Ngaq.Web/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+using Serilog.Extensions.Logging;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Ngaq.Core.Infra;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = Log.Logger = new LoggerConfiguration()
+  .Enrich.FromLogContext()
+  .WriteTo.Console()
+  .CreateLogger();
+
+//logger.Information("Starting web host");
+
+//builder.AddLoggerConfigs();
+
+// var appLogger = new SerilogLoggerFactory(logger)
+// 	.CreateLogger<Program>();
+
+//builder.Services.AddOptionConfigs(builder.Configuration, appLogger, builder);
+//builder.Services.AddServiceConfigs(appLogger, builder);
+//ServiceConfigs.AddServiceConfigs(builder.Services, appLogger, builder);
+
+//Cfg_Service.AddServiceConfigs(builder.Services, appLogger, builder);
+
+// builder.Services.AddFastEndpoints()
+// 	.SwaggerDocument(o => {
+// 		o.ShortSchemaNames = true;
+// 	})
+// ;
+
+builder.Services.ConfigureHttpJsonOptions(o=>{
+	o.SerializerOptions.TypeInfoResolver = AppJsonCtx.Default;
+	o.SerializerOptions.PropertyNamingPolicy = null;
+});
+
+var app = builder.Build();
+//app.UseFastEndpoints();
+//await app.UseAppMiddlewareAndSeedDatabase();
+app.MapGet("/", async()=>{
+// 	var ans = JSON.parse(
+// """
+// {"Email":"1","PhoneNumber":"","Password":"1","VerificationCode":"","Captcha":""}
+// """
+// 		,AppJsonCtx.Default.Req_Register
+// 	);
+
+	return "Hello World!";
+});
+
+
+app.MapPost("/Auth/Register", async(HttpContext ctx, CancellationToken ct)=>{
+	return "1";
+	// var _svc_Register = ctx.RequestServices.GetRequiredService<I_Svc_Register>();
+	// var req = await ctx.Request.ReadFromJsonAsync<Req_Register>();
+	// if(req == null){
+	// 	ctx.Response.StatusCode = 400;
+	// 	return Results.BadRequest("parameter error");
+	// }
+	// var ans = await _svc_Register.RegisterAsy(req, ct);
+	// if(ans.Ok){
+	// 	return Results.Ok(ans);
+	// }
+	// //ans.ErrToStr();
+	// return Results.BadRequest(ans);
+});
+
+app.MapPost("/Auth/Login", async(HttpContext ctx, CancellationToken ct)=>{
+	return "2";
+	// var svc_Login = ctx.RequestServices.GetRequiredService<I_Svc_Login>();
+	// var req = await ctx.Request.ReadFromJsonAsync<Req_Login>();
+	// if(req == null){
+	// 	ctx.Response.StatusCode = 400;
+	// 	return Results.BadRequest("parameter error");
+	// }
+	// var ans = await svc_Login.LoginAsy(req, ct);
+	// if(ans.Ok){
+	// 	return Results.Ok(ans);
+	// }
+	// return Results.BadRequest(ans);
+});
+
+app.Run();
+// Make the implicit Program.cs class public, so integration tests can reference the correct assembly for host building
+public partial class Program { }
