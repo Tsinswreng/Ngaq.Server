2ddb5e830e2e048b2cac7ed53286cb8966b7517f
Tsinswreng <Tsinswreng@gmail.com>
2026-04-25 19:41:13 +0800

    全局異常處理 增 日誌輸出

--- proj/Ngaq.Server.Http/GlobalErrHandler.cs
+++ proj/Ngaq.Server.Http/GlobalErrHandler.cs
@@ -4,13 +4,20 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Ngaq.Core.Infra;
 using Ngaq.Core.Infra.Errors;
 using Tsinswreng.CsErr;
 
 public class GlobalErrHandler : IExceptionHandler {
+	ILogger<GlobalErrHandler> Logger{get;}
+
+	public GlobalErrHandler(ILogger<GlobalErrHandler> Logger){
+		this.Logger = Logger;
+	}
+
 	public async ValueTask<bool> TryHandleAsync(HttpContext Ctx, Exception Err, CT Ct) {
-		System.Console.WriteLine(Err);//TODO 改用日誌
+		Logger.LogError(Err, "Unhandled exception. Path={Path}; Method={Method}", Ctx.Request.Path, Ctx.Request.Method);
 		if(Err is IAppErr E){
 			if(
 				E.Tags.Contains(AppErrTags.BizErr)
